a2a0a34b65609464543423a599402afd1c3191a0
IAMDSVSSANGRAL <havinosh.india@gmail.com>
2025-02-23 20:40:08 +0530

    23feb2023 commit

--- setup.py
+++ setup.py
@@ -1,27 +1,32 @@
 from setuptools import setup, find_packages
 from typing import List
 
-HYPEN_E_DOT='-e .'
-def get_requirements(file_path:str)->List[str]:
-    '''
-    this function will return the list of requirements
-    '''
-    requirements=[]
-    with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
+HYPEN_E_DOT = "-e ."
+
+def get_requirements(file_path: str) -> List[str]:
+    """
+    This function will return the list of requirements.
+    """
+    requirements = []
+    with open(file_path, "r", encoding="utf-8") as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.strip() for req in requirements if req.strip()]
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
-    
+
     return requirements
+
+with open("README.md", "r", encoding="utf-8") as f:
+    description = f.read()
 
 setup(
     name="havinosh_data_loader",
-    version="0.0.1",
+    version="0.0.2",
     author="Vishal Singh Sangral",
     author_email="support@havinosh.com",
     description="A Python package to dynamically load CSV files into PostgreSQL tables.",
+    long_description=description,
     long_description_content_type="text/markdown",
     url="https://github.com/yourusername/havinosh_data_loader",  # Update with actual repo
     packages=find_packages(),
